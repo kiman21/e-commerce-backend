97e5de03c16ba0c224c20564e4c911aa0e282f46
kiman21 <kim.andrew0121@gmail.com>
2023-03-23 22:04:07 -0700

    final

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:[{
+      model: Category,
+    },{
+      model: Tag,
+      as: 'product_tags',
+    }]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include:[{
+      model: Category,
+    },{
+      model: Tag,
+      as: 'product_tags',
+   }]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 // create new product
@@ -90,7 +106,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
